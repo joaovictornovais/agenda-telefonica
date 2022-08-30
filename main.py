2faa9c32207f95e4306add1d1007594f1acf5540
João Victor <joao.dipaay@gamil.com>
2022-08-29 21:54:42 -0300

    Menu adicionado

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Hello world')
+from time import sleep
+
+
+while True:
+    print('='*30)
+    print('[1] Inserir novo contato\n'
+          '[2] Consultar contato\n'
+          '[3] Alterar contato\n'
+          '[4] Remover contato\n'
+          '[5] Fechar agenda')
+    print('='*30)
+    opcao = input('Digite a opção desejada: ')
+    if opcao == '5':
+        print('Fechando agenda...')
+        sleep(1)
+        break
+
+    elif opcao == '1':
+        novoContato()
+    elif opcao == '2':
+        consultarContato()
+    elif opcao == '3':
+        alterarContato()
+    elif opcao == '4':
+        removerContato()
+    else:
+        print('ERRO: Opção inválida.')
+
+print('Agenda fechada.')
